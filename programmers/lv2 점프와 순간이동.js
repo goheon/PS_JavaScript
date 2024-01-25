777d05d7d70c742d9194301fa3272035665a72a3
goheon <gohoney22@gmail.com>
2024-01-25 11:34:47 +0900

    1/25

---
+++ programmers/lv2 점프와 순간이동.js
@@ -0,0 +1,13 @@
+function solution(n)
+{
+    let ans=0;
+    while(n>0) {
+        if(n%2==1) {
+            n=n-1;
+            ans++;
+        } else {
+            n = n/2
+        }
+    }
+    return ans;
+}
