07283186424c5717833bbc8c3f4a75563cc4be24
goheon <gohoney22@gmail.com>
2024-02-05 22:35:23 +0900

    2/5

--- programmers/단어변환 수정.js
+++ programmers/단어변환 수정.js
@@ -1,22 +1,28 @@
 function solution(begin, target, words) {
     var answer = 0;
     if (!words.includes(target)) return answer = 0;
-    answer = dfs(begin, 0);
+
+    let visited = Array(words.length).fill(0)
+    answer = dfs(begin, 0,visited);
     
     //백트래킹
-    function dfs(now, cnt) {
+    function dfs(now, cnt, visited) {
+        console.log(now,visited)
         if (now === target) {
             return cnt
         } else {
             for (let word of words) {
+                console.log(word)
                 let n = 0;
                 for (let i=0; i<word.length; i++) {
                   if (word[i]==now[i]) {
                         n++;
                     }
                 }
-             if (n === word.length-1 /*&& 방문배열 체크*/) {
-                dfs(word,cnt++)
+             if (n === word.length-1 && visited[now]===0) {
+                visited[words.indexOf(word)] = 1;
+                console.log(visited);
+                dfs(word,cnt++,visited);
                 }
             }
         }
@@ -24,3 +30,5 @@ function solution(begin, target, words) {
     
     return answer;
 }
+
+console.log(solution("hit","cog",["hot", "dot", "dog", "lot", "log", "cog"]));
