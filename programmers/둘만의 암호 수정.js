75989b70eadae942964e83584c7bf777c003fc12
goheon <gohoney22@gmail.com>
2024-02-05 21:47:50 +0900

    2/5

--- programmers/둘만의 암호 수정.js
+++ programmers/둘만의 암호 수정.js
@@ -1,41 +1,26 @@
 function solution(s, skip, index) {
-    var answer = '';
     const skipCharCode = skip.split("").map((el)=>el.charCodeAt(0));
     const sArr = s.split("");
-    console.log(skipCharCode)
+    var answer = '';
     for (char of sArr) {
         let charCode = char.charCodeAt(0);
-        let range = [];
-        
-        // 변환 범위 배열 range 생성
-        for (let i=1; i<= index; i++) {
-            if (charCode + i > 'z'.charCodeAt(0)) {
-                range.push(charCode + i - 26);
-            } else {
-                range.push(charCode + i);
+        for (let i=1; i<=index; i++) {
+            charCode +=1;
+            if (charCode > 'z'.charCodeAt(0)) {
+                charCode = charCode-26;
+            }
+            while (skipCharCode.includes(charCode)) {
+                charCode +=1;
+                if (charCode > 'z'.charCodeAt(0)) {
+                    charCode = charCode - 26;
+                }
             }
         }
-        range.sort((a,b)=>a-b);
-        skipCharCode.sort((a,b)=>a-b);
-        // skip 갯수 탐색
-        for (s of skipCharCode) {
-            if (range.includes(s)) {
-                range.shift();
-                range.push(range[range.length-1]+1);
-            }
-        }
-        console.log(charCode,range)
-        // 변환된 문자열
-        let newCharCode = range[range.length-1]
-        if (newCharCode > 'z'.charCodeAt(0)) {
-            newCharCode = newCharCode - 26;
+        if (charCode > 'z'.charCodeAt(0)) {
+            charCode = charCode - 26;
         }
         
-        let newChar =  String.fromCharCode(newCharCode);
-        answer = answer + newChar;
+        answer = answer + String.fromCharCode(charCode);
     }
     return answer;
 }
-
-
-console.log(solution('a','bc',1))
