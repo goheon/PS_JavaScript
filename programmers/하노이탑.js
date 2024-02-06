dc99354c1b15a49d3d8e4948729fd2565246994e
goheon <gohoney22@gmail.com>
2024-02-06 19:47:09 +0900

    2/6

---
+++ programmers/하노이탑.js
@@ -0,0 +1,17 @@
+function solution(n) {
+   let answer = [];
+
+   hanoi(n, 1, 3, 2);
+
+   function hanoi(num, from, to, other) {
+    if (num === 0) return;
+    hanoi(num-1, from, other, to);
+    answer.push([from,to])    
+    hanoi(num-1, other, to, from);
+
+   }
+
+    return answer;
+}
+
+console.log(solution(2));
