d8f93ab44e88e2fc8eedffa58bbf6383f1ee52e4
goheon <gohoney22@gmail.com>
2024-08-02 23:19:20 +0900

    Fri Aug 2

---
+++ boj/2798.js
@@ -0,0 +1,30 @@
+const input = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const nums = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => b - a);
+
+let diff = M;
+let max;
+for (let i = 0; i < N - 2; i++) {
+  const x = nums[i];
+  for (let j = i + 1; j < N - 1; j++) {
+    const y = nums[j];
+    for (let k = j + 1; k < N; k++) {
+      const z = nums[k];
+      let sum = x + y + z;
+      let diff = M - sum;
+      if (sum <= M && Math.abs(diff) < diff) {
+        console.log(sum, M, diff);
+        max = sum;
+      }
+    }
+  }
+}
+console.log(max);
